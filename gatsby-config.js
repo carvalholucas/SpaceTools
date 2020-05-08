befd8927e24dcbe220544ac254bd05746fd6500e
Lucas Carvalho <lucas.carvalho@azymus.com.br>
2020-05-08 17:26:58 -0300

    Change SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,6 @@ module.exports = {
         }
       }
     },
-    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -68,17 +67,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Lazytext`,
+        short_name: `lazytext`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2a2a2a`,
+        theme_color: `#2a2a2a`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`,
   ],
 }

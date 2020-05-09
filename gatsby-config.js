e3965cf90e5ae44db4de75d7654fa216b2822fa2
Lucas Carvalho <lucas.carvalho@azymus.com.br>
2020-05-09 12:24:50 -0300

    Fix gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,66 +1,71 @@
+require('dotenv').config()
+
+const pluginsConfig = [
+	`gatsby-plugin-react-helmet`,
+	`gatsby-plugin-styled-components`,
+	{
+		resolve: 'gatsby-plugin-robots-txt',
+		options: {
+			host: 'https://lazytext.netlify.app/',
+			sitemap: 'https://lazytext.netlify.app/sitemap.xml',
+			policy: [{ userAgent: '*', allow: '/' }]
+		}
+	},
+	{
+		resolve: `gatsby-plugin-google-analytics`,
+		options: {
+			trackingId: process.env.GOOGLE_ANALYTICS_ID,
+			head: false
+		},
+	},
+	{
+		resolve: `gatsby-plugin-google-adsense`,
+		options: {
+			publisherId: process.env.GOOGLE_ADSENSE
+		},
+	},
+	{
+		resolve: `gatsby-plugin-prefetch-google-fonts`,
+		options: {
+			fonts: [
+				{
+					family: `Poppins`,
+					variants: [`200`, `300`, `500`, `700`]
+				},
+			],
+		},
+	},
+	{
+		resolve: `gatsby-source-filesystem`,
+		options: {
+			name: `images`,
+			path: `${__dirname}/src/images`,
+		},
+	},
+	`gatsby-transformer-sharp`,
+	`gatsby-plugin-sharp`,
+	{
+		resolve: `gatsby-plugin-manifest`,
+		options: {
+			name: `Lazytext`,
+			short_name: `lazytext`,
+			start_url: `/`,
+			background_color: `#fff`,
+			theme_color: `#2a2a2a`,
+			display: `minimal-ui`,
+			icon: `src/images/icon.png`,
+		},
+	},
+	`gatsby-plugin-sitemap`,
+	`gatsby-plugin-offline`,
+]
+
 module.exports = {
-  siteMetadata: {
-    title: `Lazytext`,
-    description: `Free Tools online`,
-    author: `Lucas Carvalho`,
-    siteUrl: `https://lazytext.netlify.app/`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://lazytext.netlify.app/',
-        sitemap: 'https://lazytext.netlify.app/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }]
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-165826415-1",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-adsense`,
-      options: {
-        publisherId: `ca-pub-8149324393735231`
-      },
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Poppins`,
-            variants: [`200`, `300`, `500`, `700`]
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Lazytext`,
-        short_name: `lazytext`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#2a2a2a`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`,
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`,
-  ],
+	siteMetadata: {
+		title: `Lazytext`,
+		description: `Free Tools online`,
+		author: `Lucas Carvalho`,
+		siteUrl: `https://lazytext.netlify.app`,
+	},
+	plugins: pluginsConfig,
 }

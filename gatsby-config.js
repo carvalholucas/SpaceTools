21cd7cbca74d0842589dbd5abf46f2545abc3204
Lucas Carvalho <lucas.carvalho@azymus.com.br>
2020-05-09 23:56:20 -0300

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,65 +1,5 @@
 require('dotenv').config()
 
-const pluginsConfig = [
-	`gatsby-plugin-react-helmet`,
-	`gatsby-plugin-styled-components`,
-	{
-		resolve: 'gatsby-plugin-robots-txt',
-		options: {
-			host: 'https://lazytext.netlify.app/',
-			sitemap: 'https://lazytext.netlify.app/sitemap.xml',
-			policy: [{ userAgent: '*', allow: '/' }]
-		}
-	},
-	{
-		resolve: `gatsby-plugin-google-analytics`,
-		options: {
-			trackingId: process.env.GOOGLE_ANALYTICS_ID,
-			head: false
-		},
-	},
-	{
-		resolve: `gatsby-plugin-google-adsense`,
-		options: {
-			publisherId: process.env.GOOGLE_ADSENSE
-		},
-	},
-	{
-		resolve: `gatsby-plugin-prefetch-google-fonts`,
-		options: {
-			fonts: [
-				{
-					family: `Poppins`,
-					variants: [`200`, `300`, `500`, `700`]
-				},
-			],
-		},
-	},
-	{
-		resolve: `gatsby-source-filesystem`,
-		options: {
-			name: `images`,
-			path: `${__dirname}/src/images`,
-		},
-	},
-	`gatsby-transformer-sharp`,
-	`gatsby-plugin-sharp`,
-	{
-		resolve: `gatsby-plugin-manifest`,
-		options: {
-			name: `Lazytext`,
-			short_name: `lazytext`,
-			start_url: `/`,
-			background_color: `#fff`,
-			theme_color: `#2a2a2a`,
-			display: `minimal-ui`,
-			icon: `src/images/icon.png`,
-		},
-	},
-	`gatsby-plugin-sitemap`,
-	`gatsby-plugin-offline`,
-]
-
 module.exports = {
 	siteMetadata: {
 		title: `Lazytext`,
@@ -67,5 +7,63 @@ module.exports = {
 		author: `Lucas Carvalho`,
 		siteUrl: `https://lazytext.netlify.app`,
 	},
-	plugins: pluginsConfig,
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-styled-components`,
+		{
+			resolve: 'gatsby-plugin-robots-txt',
+			options: {
+				host: 'https://lazytext.netlify.app/',
+				sitemap: 'https://lazytext.netlify.app/sitemap.xml',
+				policy: [{ userAgent: '*', allow: '/' }]
+			}
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: process.env.GOOGLE_ANALYTICS_ID,
+				head: false
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-adsense`,
+			options: {
+				publisherId: process.env.GOOGLE_ADSENSE
+			},
+		},
+		{
+			resolve: `gatsby-plugin-prefetch-google-fonts`,
+			options: {
+				fonts: [
+					{
+						family: `Poppins`,
+						variants: [`200`, `300`, `500`, `700`]
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Lazytext`,
+				short_name: `lazytext`,
+				start_url: `/`,
+				background_color: `#fff`,
+				theme_color: `#2a2a2a`,
+				display: `minimal-ui`,
+				icon: `src/images/icon.png`,
+			},
+		},
+		`gatsby-plugin-sitemap`,
+		`gatsby-plugin-offline`,
+	],
 }
